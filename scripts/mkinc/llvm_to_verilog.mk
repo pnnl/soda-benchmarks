7a6b4d91ae8026d215e4c42e9857f7850b7b388b
Nicolas Agostini <n.b.agostini@gmail.com>
2025-05-29 05:50:48 +0000

    Fix bug on platform selection for openroad

--- scripts/mkinc/llvm_to_verilog.mk
+++ scripts/mkinc/llvm_to_verilog.mk
@@ -3,17 +3,25 @@
 # =============================================================================
 # Rules specific to non-optimized baseline
 $(ODIR)/bambu/baseline/06_verilog.v: $(ODIR)/05_llvm_baseline.ll
+	BAMBU_DEVICE=$(BAMBU_DEVICE) \
+	BAMBU_CLOCK_PERIOD=$(BAMBU_CLOCK_PERIOD) \
 	$(SCRIPTS_DIR)/ll_to_verilog.sh $< $@
 
 $(ODIR)/bambu/baseline/07_results.txt: $(ODIR)/05_llvm_baseline.ll
 	BAMBU_RUN_SIMULATION=true \
+	BAMBU_DEVICE=$(BAMBU_DEVICE) \
+	BAMBU_CLOCK_PERIOD=$(BAMBU_CLOCK_PERIOD) \
 	$(SCRIPTS_DIR)/ll_to_verilog.sh $< $@
 
 # =============================================================================
 # Rules specific to soda-opt optimized	
 $(ODIR)/bambu/optimized/06_verilog.v: $(ODIR)/05_llvm_optimized.ll
+	BAMBU_DEVICE=$(BAMBU_DEVICE) \
+	BAMBU_CLOCK_PERIOD=$(BAMBU_CLOCK_PERIOD) \
 	$(SCRIPTS_DIR)/ll_to_verilog.sh $< $@
 	
 $(ODIR)/bambu/optimized/07_results.txt: $(ODIR)/05_llvm_optimized.ll
 	BAMBU_RUN_SIMULATION=true \
+	BAMBU_DEVICE=$(BAMBU_DEVICE) \
+	BAMBU_CLOCK_PERIOD=$(BAMBU_CLOCK_PERIOD) \
 	$(SCRIPTS_DIR)/ll_to_verilog.sh $< $@
