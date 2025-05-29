85680d08f836f173b20fd9f33dfedd4630b1d368
Nicolas Agostini <n.b.agostini@gmail.com>
2025-05-29 00:30:57 +0000

    Add verilog synthesis and verilator simulator through bambu - baseline

---
+++ scripts/mkinc/soda_to_llvm.mk
@@ -0,0 +1,44 @@
+# This file provides rules to generate LLVM IR using SODA-OPT
+
+
+# =============================================================================
+# Rules specific to non-optimized Baseline
+$(ODIR)/04_llvm_baseline.mlir: $(ODIR)/02_linalg.mlir $(SCRIPTS_DIR)/soda_to_llvm_baseline.sh
+	$(SCRIPTS_DIR)/soda_to_llvm_baseline.sh $< $@
+	mv forward_kernel_testbench.c $(ODIR)/forward_kernel_testbench.c
+
+$(ODIR)/05_llvm_baseline.ll: $(ODIR)/04_llvm_baseline.mlir
+	mlir-translate $< -o $@ --mlir-to-llvmir
+
+# =============================================================================
+# Rules specific to soda-opt optimized
+$(ODIR)/04_llvm_optimized.mlir: $(ODIR)/02_linalg.mlir $(SCRIPTS_DIR)/soda_to_llvm_optimized.sh
+	$(SCRIPTS_DIR)/soda_to_llvm_optimized.sh $< $@
+	soda-opt $< -o $@ -soda-opt-pipeline-for-bambu=use-bare-ptr-memref-call-conv
+	mv forward_kernel_testbench.c $(ODIR)/forward_kernel_testbench.c
+
+$(ODIR)/05_llvm_optimized.ll: $(ODIR)/04_llvm_optimized.mlir
+	mlir-translate $< -o $@ --mlir-to-llvmir
+
+# =============================================================================
+# Rules specific mlir transformed with a transform dialect library
+$(ODIR)/04_llvm_transformed.mlir: $(ODIR)/03-03_linalg_isolated.mlir
+	soda-opt $< -o $@ \
+		--transform-interpreter \
+		--soda-opt-pipeline-for-bambu=use-bare-ptr-memref-call-conv \
+		--transform-preload-library=transform-library-paths="$(SODA_OPT_TRANSFORM_LIB)" \
+		--soda-transform-erase-schedule \
+		-lower-all-to-llvm=use-bare-ptr-memref-call-conv
+
+$(ODIR)/05_llvm_transformed.ll: $(ODIR)/04_llvm_transformed.mlir
+	mlir-translate $< -o $@ --mlir-to-llvmir
+
+# Checks if the first generated file exists, if so, delete all generated files
+# This is to avoid deleting files from other directories
+# PHONY: clean
+# clean:
+# 	test -f $(ODIR)/03-01_linalg_searched.mlir && \
+# 	rm -f $(ODIR)/*.mlir && \
+# 	rm -f $(ODIR)/*.ll && \
+# 	rm -r $(ODIR)
+# 	rm -f core
---
+++ scripts/soda_to_llvm_baseline.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+# Check if docker is available or if the needed binaries are available
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source $SCRIPT_DIR/check_docker.sh
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <input_linalg.mlir> <output_llvm.mlir>" >&2
+  exit 1
+fi
+
+OUTPUT_DIR=$(pwd)/$(dirname $2)
+mkdir -p $OUTPUT_DIR
+OUTPUT_NAME=$(basename $2)
+OUTPUT_PATH=$OUTPUT_DIR/$OUTPUT_NAME
+
+# set -x
+
+# $DOCKER_RUN \
+# mlir-opt \
+#   -convert-linalg-to-affine-loops \
+#   -expand-strided-metadata \
+#   -lower-affine \
+#   -convert-scf-to-cf \
+#   -convert-complex-to-standard \
+#   -convert-vector-to-llvm \
+#   --convert-math-to-llvm \
+#   --convert-math-to-libm \
+#   -arith-expand \
+#   -memref-expand \
+#   -convert-to-llvm="filter-dialects=memref" \
+#   -finalize-memref-to-llvm \
+#   -convert-arith-to-llvm \
+#   -finalize-memref-to-llvm \
+#   -convert-complex-to-llvm \
+#   -convert-func-to-llvm='use-bare-ptr-memref-call-conv=1' \
+#   --test-lower-to-llvm \
+#   -convert-cf-to-llvm \
+#   -reconcile-unrealized-casts \
+#   -symbol-dce \
+#   $1 \
+#   -o $2
+
+# $DOCKER_RUN \
+# soda-opt \
+#   -convert-operation-to-soda="anchor-op=linalg.batch_matmul" \
+#   -soda-outline-bambu-code \
+#   -soda-extract-arguments-to-c-testbench=using-bare-ptr \
+#   -soda-generate-bambu-accelcode=no-aa \
+#   -lower-all-to-llvm=use-bare-ptr-memref-call-conv \
+#   $1 \
+#   -o $2
+
+$DOCKER_RUN \
+soda-opt \
+  --convert-all-to-soda \
+  -soda-outline-bambu-code \
+  -soda-extract-arguments-to-c-testbench=using-bare-ptr \
+  -soda-generate-bambu-accelcode=no-aa \
+  -lower-all-to-llvm=use-bare-ptr-memref-call-conv \
+  -mlir-print-ir-after-all \
+  $1 \
+  -o $2 \
+  2>&1 | cat > $2.steps.mlir
+
+# $DOCKER_RUN \
+# soda-opt \
+#   --lower-all-to-llvm \
+#   $1 \
+#   -o $2
+  
+set +x
